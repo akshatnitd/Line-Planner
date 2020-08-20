9a2584a79301369caae062f1e08f77a68bca8a6f
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-08-20 16:38:02 +0000

    Added navigation and geolocate control to map, assigning positions. Assigned logo position to top-right. Overode top-left and top-right positions.

--- assets/js/mapboxCustom.js
+++ assets/js/mapboxCustom.js
@@ -7,5 +7,9 @@ $("document").ready(function () {
     style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
     center: [-2.995859, 53.405974], // starting position [lng, lat]
     zoom: 11, // starting zoom
+    logoPosition: "top-right",
   });
+
+  map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+  map.addControl(new mapboxgl.GeolocateControl(), "top-left");
 });
