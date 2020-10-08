47e0933014f2cf01dfa29019108761d0d49301f3
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-10-08 17:42:25 +0000

    Commented out broken JS code in UTMproject.js

--- assets/js/UTMproject.js
+++ assets/js/UTMproject.js
@@ -1,6 +1,6 @@
 import LatLon, { Cartesian, Vector3d, Dms } from 'https://cdn.jsdelivr.net/npm/geodesy@2/latlon-ellipsoidal.js';
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#map").on("mousemove", function(){
         let cursorLatLon = new LatLon($("#cursorLat").children("p").children("span").html(), $("#cursorLong").children("p").children("span").html(),);
     });
-})
+})*/
