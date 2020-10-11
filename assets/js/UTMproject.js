da81152dc58cb86d49083dd046a127534ca5a923
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-10-11 12:45:01 +0000

    revised module import pathway to correct script in UTMproject.js. declared variable 'UTM' and entered function to convert 'cursorLatLong to UTM coordinates.

--- assets/js/UTMproject.js
+++ assets/js/UTMproject.js
@@ -1,8 +1,10 @@
-import LatLon, { Cartesian, Vector3d, Dms } from 'https://cdn.jsdelivr.net/npm/geodesy@2/latlon-ellipsoidal.js';
+import { LatLon } from 'https://cdn.jsdelivr.net/npm/geodesy@2/utm.js';
 $(document).ready(function(){
     $("#map").on("mousemove", function(){
         let cursorLat = $("#cursorLat>p>span").html();
         let cursorLong = $("#cursorLong>p>span").html();
         let cursorLatLong = new LatLon(cursorLat, cursorLong,);
+        let UTM = cursorLatLong.toUtm();
+        console.log(UTM);
     });
 })
