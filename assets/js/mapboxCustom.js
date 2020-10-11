7ad54fec793ff908c117e15f07a8a49cd94640f4
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-10-11 10:10:01 +0000

    included return of latitude and longitude variables in 'reportCursorPos' function within mapboxCustom.js

--- assets/js/mapboxCustom.js
+++ assets/js/mapboxCustom.js
@@ -61,6 +61,8 @@ $("document").ready(function () {
             '<p class="no-margin">LAT: ' + '<span>' + latitude + '</span>' + '</p>'
             document.getElementById("cursorLong").innerHTML =
             '<p class="no-margin">LONG: ' + '<span>' +  longitude + '</span>' + '</p>'
+            return(latitude);
+            return(longitude);
         });
     };
     reportCursorPos();
