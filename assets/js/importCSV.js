ba7288b402208ed6f54e529d94f0dfaf9784213e
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-10-25 15:53:20 +0000

    Created function to convert the string based array into a nested floating number array.

--- assets/js/importCSV.js
+++ assets/js/importCSV.js
@@ -15,16 +15,33 @@ $("document").ready(function () {
             //When reader has successfully completed and passed the load event the result is written to the "readBoundaryResult" variable.
             reader.onload = function(e) {
                 let readBoundaryResult = reader.result;
-                console.log(readBoundaryResult);
                 textToArray(readBoundaryResult);
             }
         }
-        //Function to turn text string into an array, splitting by newline.
+        //Function to turn text string into an array splitting by newline, and iterate through this array to build a new array which ignores the headers and parses the string items as floating numbers.
         function textToArray (textToSplit) {
+            //Splits FileReader result by new line. 
             let importedBoundaryArray = [];
             let importedBoundarySplit = textToSplit.split("\n");
             importedBoundaryArray.push(importedBoundarySplit);
-            console.log(importedBoundaryArray);
+            //Splits Array created from initial newline split by comma, and iterates through each nested array to parse the string as a float, before writing this to a master array of coordinates. 
+            let vertexBoundaryArray = [];
+            for (let i = 1; i < importedBoundaryArray[0].length; i++) {
+                let vertexArray = [];
+                //Splits string array
+                let boundaryArraySplit = importedBoundaryArray[0][i].split(",");
+                for (let j = 0; j < boundaryArraySplit.length; j++) {
+                    let vertexCoords = [];
+                    //Parses each split array element as a floating number. 
+                    let vertexParam = parseFloat(boundaryArraySplit[j]);
+                    //Pushes lat and long for each coordinate back into thier own array.
+                    vertexCoords.push(vertexParam);
+                    //Combined coordinates into a single array for the vertex. 
+                    vertexArray.push(vertexCoords);
+                }
+                //Pushes the combined coordinate as nested arrays within a master array for the boundary. 
+                vertexBoundaryArray.push(vertexArray);
+            }
         }
     });
 });
