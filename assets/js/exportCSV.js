6298e20188b5128ab86a7f5479863dbfab4dcbad
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-11-01 12:27:08 +0000

    Copied boundary export projected csv script and adapted to export line projected csv.

--- assets/js/exportCSV.js
+++ assets/js/exportCSV.js
@@ -1,6 +1,6 @@
 $("document").ready(function () {
     //Adapted from https://www.codexworld.com/export-html-table-data-to-csv-using-javascript/
-    $("#boundaryExportGeo").click(function () {
+  $("#boundaryExportGeo").click(function () {
     function exportBoundaryGeoToCSV() {
       let csv = [];
       let rows = document.querySelectorAll("#boundaryTable tr");
@@ -75,6 +75,33 @@ $("document").ready(function () {
         csvFile = new Blob([csv], {type: "text.csv"});
         downloadLink = document.createElement("a");
         downloadLink.download = "lineExportGeodetic.csv";
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+    }
+  });
+  $("#lineExportProjected").click(function () {
+    function exportLineProjectedToCSV() {
+      let csv = [];
+      let rows = document.querySelectorAll("#lineConverted tr");
+
+      for (let i = 0; i < rows.length; i++) {
+        let row = [],
+          cols = rows[i].querySelectorAll("td, th");
+        for (let j = 0; j < cols.length; j++) row.push(cols[j].innerText);
+
+        csv.push(row.join(","));
+      }
+      downloadLineProjectedCSV(csv.join("\n"));
+    }
+    exportLineProjectedToCSV();
+    function downloadLineProjectedCSV(csv) {
+        let csvFile;
+        let downloadLink;
+        csvFile = new Blob([csv], {type: "text.csv"});
+        downloadLink = document.createElement("a");
+        downloadLink.download = "boundaryLineProjected.csv";
         downloadLink.href = window.URL.createObjectURL(csvFile);
         downloadLink.style.display = "none";
         document.body.appendChild(downloadLink);
