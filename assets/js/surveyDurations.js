e385525617cc2cb65705d3ea451c02f78ce28a05
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-11-07 15:57:55 +0000

    Moved script to calculate total survey duration within the if statement querying if there are lines so that it only triggers if both the acquistion duration and turn duration is successfully calculated first.

--- assets/js/surveyDurations.js
+++ assets/js/surveyDurations.js
@@ -47,6 +47,7 @@ $("document").ready(function(){
             let noTurns = parseInt(document.getElementById("lineCount").innerHTML);
             //If lines are present.
             if(noTurns > 0) {
+                $("#recalcWarning").hide();
                 //Calculate the turn time and converts to decimal hours. No. of turns in 1x less than the number of lines. 
                 let turnDurationDecHours = ((noTurns-1)*perTurnTime)/60;
                 //Pushes turn decimal hours into an array for total hours calculations. 
@@ -61,25 +62,25 @@ $("document").ready(function(){
                 let turnDurationSecs = Math.round((turnDurationDecMinutes % 1)*60);
                 //Writes a HTML string for this computed turn duration.
                 $("#turnTotalTime").html(" " + turnDurationHours + " hrs " + turnDurationMinutes + " mins " + turnDurationSecs + " secs ");
+            //Script to calculate Total Survey Duration.
+                //Uses array which the 2x subtotals were pushed to above and adds the array values together.
+                let totalDurationDecHours = durationsDecHoursArray.reduce(function(a, b){
+                    return a + b;
+                }, 0);
+                //Gets just hours from the computed duration.
+                let totalDurationHours = Math.floor(totalDurationDecHours);
+                //Gets just decimal minutes from the computed duration.
+                let totalDurationDecMinutes = (totalDurationDecHours % 1)*60;
+                //Gets just minutes from the computed duration.
+                let totalDurationMinutes = Math.floor(totalDurationDecMinutes);
+                //Gets just seconds from the computed duration and rounds to the nearest second. 
+                let totalDurationSecs = Math.round((totalDurationDecMinutes % 1)*60);
+                //Writes a HTML string for this computed total duration.
+                $("#totalTime").html(" " + totalDurationHours + " hrs " + totalDurationMinutes + " mins " + totalDurationSecs + " secs ");
             } else {}
         } else {
             //Alert if Est. time between lines is not a number. 
             alert("Turn time is not a number"); 
         }
-    //Script to calculate Total Survey Duration.
-        //Uses array which the 2x subtotals were pushed to above and adds the array values together.
-        let totalDurationDecHours = durationsDecHoursArray.reduce(function(a, b){
-            return a + b;
-        }, 0);
-        //Gets just hours from the computed duration.
-        let totalDurationHours = Math.floor(totalDurationDecHours);
-        //Gets just decimal minutes from the computed duration.
-        let totalDurationDecMinutes = (totalDurationDecHours % 1)*60;
-        //Gets just minutes from the computed duration.
-        let totalDurationMinutes = Math.floor(totalDurationDecMinutes);
-        //Gets just seconds from the computed duration and rounds to the nearest second. 
-        let totalDurationSecs = Math.round((totalDurationDecMinutes % 1)*60);
-        //Writes a HTML string for this computed total duration.
-        $("#totalTime").html(" " + totalDurationHours + " hrs " + totalDurationMinutes + " mins " + totalDurationSecs + " secs ");
     });
 })
