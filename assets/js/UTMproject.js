5f185ef7b18d69441f6f4f7e9b974fb2897d98b4
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-11-08 13:55:33 +0000

    Added in script to convert last touch position geodetic to projected in UTMproject.js.

--- assets/js/UTMproject.js
+++ assets/js/UTMproject.js
@@ -1,6 +1,40 @@
 import { LatLon } from "https://cdn.jsdelivr.net/npm/geodesy@2/utm.js";
 $(document).ready(function () {
+// Script to convert cursor position to UTM
   $("#map").on("mousemove", function () {
+    let cursorLat = $("#cursorLat>p>span").html();
+    let cursorLong = $("#cursorLong>p>span").html();
+    let cursorLatLong = new LatLon(cursorLat, cursorLong);
+    let UTM = cursorLatLong.toUtm();
+    let northing = UTM["northing"];
+    let easting = UTM["easting"];
+    let hemi = UTM["hemisphere"];
+    let zone = UTM["zone"];
+    document.getElementById("cursorGeodetic").innerHTML =
+      '<p class="no-margin">' + "<span> WGS84 </span>" + "</p>";
+    document.getElementById("cursorNorthing").innerHTML =
+      '<p class="no-margin">' +
+      "<span>NORTHING: " +
+      northing.toFixed(2) +
+      "</span>" +
+      "</p>";
+    document.getElementById("cursorEasting").innerHTML =
+      '<p class="no-margin">' +
+      "<span>EASTING: " +
+      easting.toFixed(2) +
+      "</span>" +
+      "</p>";
+    document.getElementById("cursorUtmZone").innerHTML =
+      '<p class="no-margin">' +
+      "<span> UTM " +
+      zone +
+      " " +
+      hemi +
+      "</span>" +
+      "</p>";
+  });
+//Script to convert last touch position to UTM
+  $("#map").on("touchend", function () {
     let cursorLat = $("#cursorLat>p>span").html();
     let cursorLong = $("#cursorLong>p>span").html();
     let cursorLatLong = new LatLon(cursorLat, cursorLong);
